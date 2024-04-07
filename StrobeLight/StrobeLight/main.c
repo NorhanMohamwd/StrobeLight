ef14d65bd44699eb7301edc4529a7acb47db89be
Norhan Mohamed <norhanmohamwd@gmail.com>
2024-04-07 17:57:33 +0200

    Create: microchip project

---
+++ StrobeLight/StrobeLight/main.c
@@ -0,0 +1,19 @@
+/*
+ * StrobeLight.c
+ *
+ * Created: 4/7/2024 5:56:25 PM
+ * Author : nora
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		/*output function*/
+    }
+}
+
