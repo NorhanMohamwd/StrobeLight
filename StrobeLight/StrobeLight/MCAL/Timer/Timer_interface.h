1b6f6fbbfc398e67f70677284116f0eb7e6b2ffc
Norhan Mohamed <norhanmohamwd@gmail.com>
2024-04-10 02:48:03 +0200

    update: timer module
    added: configuration for TIMERB

--- StrobeLight/StrobeLight/MCAL/Timer/Timer_interface.h
+++ StrobeLight/StrobeLight/MCAL/Timer/Timer_interface.h
@@ -11,10 +11,11 @@
 #include "StdTypes.h"
 #include "Timer_config.h"
 
-//static volatile void (*g_timerCallBackPtr)(void)=NULLPTR;
+static volatile void (*g_timerCallBackPtr)(void)=NULLPTR;
 uint8_t gl_prescaler;
 
 #define CPU_FREQUENCY	20000000
+#define REQUIRED_TICKS  55000
 
 #define SPLITM		    0 
 #define DIR	            0
@@ -22,11 +23,20 @@ uint8_t gl_prescaler;
 #define ENABLE			0
 #define CMD1			3
 #define CMD0			2
+#define CAPT			0
+
 
 
 typedef enum{
-	NORMAL_MODE
+	TIMERA,
+	TIMERB
+}Timer_timerType;
+
+typedef enum{
+	NORMAL_MODE =0,
+	PERIDIC_INTERRUPT =0
 }Timer_modeType;
+
 
 typedef enum{
 	DIV1,
@@ -36,7 +46,11 @@ typedef enum{
 	DIV16,
 	DIV64,
 	DIV256,
-	DIV1024
+	DIV1024,
+	CLKDIV1=0,	/*for TIMERB*/
+	CLKDIV2=1,
+	CLKTCA=3
+	
 }Timer_prescaler;
 
 typedef enum{
@@ -47,16 +61,21 @@ typedef enum{
 
 
 typedef struct {
+	Timer_timerType timer;
 	Timer_modeType mode;
 	Timer_prescaler prescaler;
 	Timer_countDirection direction;
 }Timer_configType;
 
+Timer_timerType timerName;
+
 void Timer_init(const Timer_configType * Config_ptr);
 void Timer_setDelay(uint32_t ms);
-void Timer_disable(void);
-void Timer_enable(void);
+void Timer_disable(Timer_timerType timerName);
+void Timer_enable(Timer_timerType timerName);
 bool_t Timer_getFlag(void);
+void TimerB_setCallBack(void (*a_ptr)(void));
+void Timer_resetWDG(void);
 /*
 typedef enum{
 	NONE,
--- StrobeLight/StrobeLight/MCAL/Timer/Timer_program.c
+++ StrobeLight/StrobeLight/MCAL/Timer/Timer_program.c
@@ -5,29 +5,45 @@
  *  Author: Norhan Mohamed - https://github.com/NorhanMohamwd
  */ 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include "Timer_interface.h"
 #include "StdMacros.h"
 #include "StdTypes.h"
 
 void Timer_init(const Timer_configType * Config_ptr){
-	
-	Timer_disable();
-	if (Config_ptr-> mode == NORMAL_MODE)
-	{
-		CLEAR_BIT(TCA0_SINGLE_CTRLB,SPLITM);	/*disable split mode*/
-		TCA0_SINGLE_CTRLB = (TCA0_SINGLE_CTRLB & 0xF8) | (Config_ptr->mode);
-		
-		if(Config_ptr->prescaler < 8 &&  Config_ptr->prescaler > -1)
+	if (Config_ptr-> timer == TIMERA)
+	{	timerName = TIMERA;
+		Timer_disable(timerName);
+		if (Config_ptr-> mode == NORMAL_MODE)
 		{
-			TCA0_SINGLE_CTRLA = (TCA0_SINGLE_CTRLA & 0xF1) | (Config_ptr->prescaler <<1);
-			gl_prescaler = Config_ptr->prescaler;
+			CLEAR_BIT(TCA0_SINGLE_CTRLB,SPLITM);	/*disable split mode*/
+			TCA0_SINGLE_CTRLB = (TCA0_SINGLE_CTRLB & 0xF8) | (Config_ptr->mode);
+		
+			if(Config_ptr->prescaler < 8 &&  Config_ptr->prescaler > -1)
+			{
+				TCA0_SINGLE_CTRLA = (TCA0_SINGLE_CTRLA & 0xF1) | (Config_ptr->prescaler <<1);
+				gl_prescaler = Config_ptr->prescaler;
 			
-			if (Config_ptr->direction == COUNTING_DOWN)	
-			SET_BIT(TCA0_SINGLE_CTRLESET,DIR);
-			else if (Config_ptr->direction == COUNTING_UP)
-			SET_BIT(TCA0_SINGLE_CTRLECLR,DIR);
+				if (Config_ptr->direction == COUNTING_DOWN)	
+				SET_BIT(TCA0_SINGLE_CTRLESET,DIR);
+				else if (Config_ptr->direction == COUNTING_UP)
+				SET_BIT(TCA0_SINGLE_CTRLECLR,DIR);
 			
-			//SET_BIT(TCA0_SINGLE_INTCTRL,OVF);
+				//SET_BIT(TCA0_SINGLE_INTCTRL,OVF);
+			}
+		}
+	}
+	else if (Config_ptr->timer == TIMERB)
+	{
+		timerName = TIMERB;
+		Timer_disable(timerName);
+		if (Config_ptr-> mode == PERIDIC_INTERRUPT)
+		{
+			TCB0_CTRLB = ( TCB0_CTRLB & 0xF8) | (Config_ptr->mode);
+			TCB0_CTRLA = ( TCB0_CTRLA & 0xF9) | (Config_ptr->prescaler <<1);
+			SET_BIT(TCB0_INTCTRL,CAPT);
+			TCB0_CCMP = REQUIRED_TICKS;
+			Timer_enable(timerName);
 		}
 	}
 }
@@ -90,17 +106,33 @@ void Timer_setDelay(uint32_t ms)
 	TCA0_SINGLE_PER = ms / timeTick_ms;
 	
 	/*restart counter*/
-	//SET_BIT(TCA0_SINGLE_CTRLESET,CMD1);
-	//SET_BIT(TCA0_SINGLE_CTRLECLR,CMD0);
+	SET_BIT(TCA0_SINGLE_CTRLESET,CMD1);
+	SET_BIT(TCA0_SINGLE_CTRLECLR,CMD0);
 }
 
-void Timer_disable(void)
+void Timer_disable(Timer_timerType timerName)
 {
-	CLEAR_BIT(TCA0_SINGLE_CTRLA,ENABLE);
+	if (timerName == TIMERA)
+	{
+		CLEAR_BIT(TCA0_SINGLE_CTRLA,ENABLE);
+	}
+	else if (timerName == TIMERB)
+	{
+		CLEAR_BIT(TCB0_CTRLA,ENABLE);
+	}
+	
 }
-void Timer_enable(void)
+void Timer_enable(Timer_timerType timerName)
 {
-	SET_BIT(TCA0_SINGLE_CTRLA,ENABLE);
+	if (timerName == TIMERA)
+	{
+		SET_BIT(TCA0_SINGLE_CTRLA,ENABLE);
+	}
+	else if (timerName == TIMERB)
+	{
+		SET_BIT(TCB0_CTRLA,ENABLE);
+	}
+	
 }
 
 bool_t Timer_getFlag(void)
@@ -115,15 +147,25 @@ bool_t Timer_getFlag(void)
 		return FALSE;
 	}
 }
-/*
-void Timer_setCallBack(void (*a_ptr)(uint16_t)){
+
+void TimerB_setCallBack(void (*a_ptr)(void)){
 	
 	g_timerCallBackPtr=a_ptr;
 }
 
-ISR(TCA0_OVF_vect){
+void Timer_resetWDG(void){
+	static uint8_t overFlows =0;
+	overFlows++;
+	if (overFlows==15)
+	{
+		overFlows=0;
+		/*reset wdg*/
+	}
+}
+
+ISR(TCB0_INT_vect){
 	if(g_timerCallBackPtr != NULLPTR){
 		(*g_timerCallBackPtr)();
 	}
 }
-*/
+
