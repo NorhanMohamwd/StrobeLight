be48581adba9d41a50b2368a10384d24cf6f64aa
Norhan Mohamed <norhanmohamwd@gmail.com>
2024-04-10 02:41:06 +0200

    updaate: button module
    added: button_init()
                button_read()

--- StrobeLight/StrobeLight/HAL/Button/Button.c
+++ StrobeLight/StrobeLight/HAL/Button/Button.c
@@ -4,3 +4,40 @@
  * Created: 4/7/2024 9:05:12 PM
  *  Author: Norhan Mohamed - https://github.com/NorhanMohamwd
  */ 
+#include "Button.h"
+
+
+
+void button_init(void){
+	dio_initPin(LEFT_IN,INPULL);
+	dio_initPin(RIGHT_IN,INPULL);
+	dio_initPin(BACK_IN,INPULL);
+	dio_initPin(BRAKE_IN,INPULL);
+}
+
+union signalsUnion button_read(void){
+
+	uint8_t currentButtonRead= ((dio_readPort(PB) & 0xF8) | (dio_readPin(PINC0)<<3) );
+	static  uint8_t previousButtonStage = 0xFF;
+	uint8_t diffButtonStage = currentButtonRead ^ previousButtonStage;
+	current.value=currentButtonRead;
+	diff.value= diffButtonStage;
+			
+	if (current.signal.LEFT == LOW && diff.signal.LEFT == HIGH ){
+		result.signal.LEFT = HIGH;
+	}
+	else if (current.signal.RIGHT == LOW && diff.signal.RIGHT == HIGH){
+		result.signal.RIGHT = HIGH;
+	}
+	else if (current.signal.BACK == LOW && diff.signal.BACK == HIGH){
+		result.signal.BACK = HIGH;
+	}
+	else if (current.signal.BRAKE == LOW && diff.signal.BRAKE == HIGH){
+		result.signal.BRAKE = HIGH;
+	}
+			
+	previousButtonStage = currentButtonRead;
+	return result;
+}
+
+	
--- StrobeLight/StrobeLight/HAL/Button/Button.h
+++ StrobeLight/StrobeLight/HAL/Button/Button.h
@@ -9,8 +9,35 @@
 #ifndef BUTTON_H_
 #define BUTTON_H_
 
+#include "Delay.h"
+#include "Dio_interface.h"
 
 
+union signalsUnion{
+	volatile struct  {
+		uint8_t  LEFT      : 1;
+		uint8_t  RIGHT     : 1;
+		uint8_t  BRAKE     : 1;
+		uint8_t  BACK      : 1;
+	} signal;
+	uint8_t value ;
+};
+
+union signalsUnion current;
+union signalsUnion diff;
+union signalsUnion result;
+
+typedef enum
+{
+	LEFT_IN=PINC0,
+	RIGHT_IN=PINB0,
+	BACK_IN=PINB1,
+	BRAKE_IN=PINB2
+ } button_t ;
+
+
+void button_init(void);
+union signalsUnion button_read(void);
 
 
 #endif /* BUTTON_H_ */
