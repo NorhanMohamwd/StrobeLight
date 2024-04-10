01b40f3d9d9acb9c627eb9bbc15f28a2f0e02b12
Norhan Mohamed <norhanmohamwd@gmail.com>
2024-04-10 02:42:58 +0200

    update: delay module
    added: delay_init()
                delay_ms()

---
+++ StrobeLight/StrobeLight/HAL/Delay/Delay.c
@@ -0,0 +1,28 @@
+/*
+ * Delay.c
+ *
+ * Created: 4/8/2024 1:27:01 PM
+ *  Author: Norhan Mohamed - https://github.com/NorhanMohamwd
+ */ 
+#include "Delay.h"
+#include "Timer_interface.h"
+
+void delay_init(void)
+{
+	Timer_configType configType={TIMERA,NORMAL_MODE,DIV1024,COUNTING_UP};
+	Timer_init(&configType);
+}
+
+void delay_ms(uint32_t ms)
+{	static uint8_t getFlag =0;
+	timerName=TIMERA;
+	Timer_setDelay(ms);
+	Timer_enable(timerName);
+	getFlag= Timer_getFlag();
+	while(getFlag==FALSE){
+		getFlag= Timer_getFlag();
+	}
+	getFlag=FALSE;
+	
+	Timer_disable(timerName);
+}
---
+++ StrobeLight/StrobeLight/HAL/Delay/Delay.h
@@ -0,0 +1,19 @@
+/*
+ * Delay.h
+ *
+ * Created: 4/8/2024 1:27:17 PM
+ *  Author: Norhan Mohamed - https://github.com/NorhanMohamwd
+ */ 
+
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+
+#include "StdTypes.h"
+
+void delay_init(void);
+void delay_ms(uint32_t ms);
+
+
+#endif /* DELAY_H_ */
