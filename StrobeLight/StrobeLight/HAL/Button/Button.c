cc24548b2a621815ec9b935a5b1d184ccefb8624
Norhan Mohamed <norhanmohamwd@gmail.com>
2024-04-24 01:30:44 +0200

    update: button module
    changed the logic to interrupt-based

--- StrobeLight/StrobeLight/HAL/Button/Button.c
+++ StrobeLight/StrobeLight/HAL/Button/Button.c
@@ -8,6 +8,15 @@
 #include "Dio_interface.h"
 #include "Dio_config.h"
 #include "Delay.h"
+#include "Timer_interface.h"
+
+static void (*g_buttonCallBackPtr)(union signalsUnion)=NULLPTR;
+
+dio_pinInterrupt left_edge = FALLING;
+dio_pinInterrupt brake_edge = FALLING;
+
+union signalsUnion current;
+union signalsUnion result;
 
 
 
@@ -18,59 +27,103 @@ void button_init(void){
 	dio_initPin(PB,BRAKE_IN,INPULL);
     dio_initPin(PB,BUTTON_1,INPULL);
 	dio_initPin(PB,BUTTON_2,INPULL);
-    
+	
+	dio_enableInterruptTrigger(PC,LEFT_IN,FALLING);
+	dio_enableInterruptTrigger(PB,RIGHT_IN,LEVEL);
+	dio_enableInterruptTrigger(PB,BACK_IN,LEVEL);
+	dio_enableInterruptTrigger(PB,BRAKE_IN,FALLING);
+	dio_enableInterruptTrigger(PB,BUTTON_1,LEVEL);
+	dio_enableInterruptTrigger(PB,BUTTON_2,LEVEL);
 }
 
-union signalsUnion button_read(void){
-	dio_pinVoltage_t pinValue = dio_readPin(PC,LEFT_IN);
-    uint8_t portValue = dio_readPort(PB);
-	uint8_t currentButtonRead= (( portValue & 0x1F) | (pinValue<<5) ); /*puts the six readings into one variable*/
-	static  uint8_t previousButtonStage = 0xFF;								/*initialize the previous state with HIGH*/
-	uint8_t diffButtonStage = currentButtonRead ^ previousButtonStage;		/*to process only the changed value and discard old ones*/
-	result.value = 0;
-	current.value = currentButtonRead;
-	diff.value = diffButtonStage;
+void button_detectPress(void){
+	uint8_t flags = (dio_readFlags(PB) & 0x1F ) | (dio_readFlags(PC)<<5) ;  
+	current.value = flags;
 	
-	/*checks that it's a new value and that it's low*/
-	if ((current.signal.LEFT == LOW ) && (diff.signal.LEFT == HIGH ) ){	
-        delay_ms(50);
-        if (dio_readPin(PC,LEFT_IN)==LOW){
-        	result.signal.LEFT = HIGH;
-        }
+	if (current.signal.LEFT == HIGH )
+	{	
+		dio_resetFlags(PC,LEFT_IN);
+		if (left_edge == FALLING)
+		{
+			button_overFlows=0;
+			dio_enableInterruptTrigger(PC,LEFT_IN,RISING);
+			left_edge = RISING;
+		}
+		else if (left_edge== RISING)
+		{
+			if (button_overFlows < FIVE_SEC_COUNT){
+				result.signal.LEFT_PRESSED=SHORT_PRESS;
+				result.signal.arrOfPresses[LEFT_IN] += 1;
+				result.signal.AUTO_MODE = NO_AUTO_MODE;
+			}
+			else {
+				result.signal.LEFT_PRESSED=LONG_PRESS;
+				result.signal.AUTO_MODE = AUTO_MODE_;
+			}
+			dio_enableInterruptTrigger(PC,LEFT_IN,FALLING);
+			left_edge = FALLING;
+			result.signal.LEFT=HIGH;
+		}
 	}
-	else if ((current.signal.RIGHT == LOW ) && (diff.signal.RIGHT == HIGH )){
-        delay_ms(50);
-        if (dio_readPin(PB,RIGHT_IN)==LOW){
-        	result.signal.RIGHT = HIGH;
-        }
+	else if (current.signal.RIGHT == HIGH )
+	{
+		dio_resetFlags(PB,RIGHT_IN);
+		result.signal.RIGHT=HIGH;
+		result.signal.arrOfPresses[RIGHT_IN] += 1;
+		result.signal.AUTO_MODE = NO_AUTO_MODE;
 	}
-	else if ((current.signal.BACK == LOW ) && (diff.signal.BACK == HIGH )){
-        delay_ms(50);
-        if (dio_readPin(PB,BACK_IN)== LOW){
-            result.signal.BACK = HIGH;
-        }
+	else if (current.signal.BACK == HIGH )
+	{
+		dio_resetFlags(PB,BACK_IN);
+		result.signal.BACK=HIGH;
+		result.signal.arrOfPresses[BACK_IN] += 1;
+		result.signal.AUTO_MODE = NO_AUTO_MODE;
 	}
-	else if ((current.signal.BRAKE == LOW ) && (diff.signal.BRAKE == HIGH )){
-        delay_ms(50);
-        if (dio_readPin(PB,BRAKE_IN)== LOW){
-      		result.signal.BRAKE = HIGH;
-        }
+	else if (current.signal.BRAKE == HIGH )
+	{
+		dio_resetFlags(PB,BRAKE_IN);
+		if (brake_edge == FALLING)
+		{
+			button_overFlows=0;
+			dio_enableInterruptTrigger(PB,BRAKE_IN,RISING);
+			brake_edge = RISING;
+		}
+		else if (brake_edge== RISING)
+		{
+			if (button_overFlows < FIVE_SEC_COUNT){
+				result.signal.BRAKE_PRESSED=SHORT_PRESS;
+				result.signal.arrOfPresses[BRAKE_IN] += 1;
+				result.signal.AUTO_MODE = NO_AUTO_MODE;
+			}
+			else {
+				result.signal.BRAKE_PRESSED=LONG_PRESS;
+				result.signal.AUTO_MODE = NO_AUTO_MODE;
+			}
+			dio_enableInterruptTrigger(PB,BRAKE_IN,FALLING);
+			brake_edge = FALLING;
+			result.signal.BRAKE=HIGH;
+		}
 	}
-    else if ((current.signal.BTN_1 == LOW ) && (diff.signal.BTN_1 == HIGH )){
-        delay_ms(50);
-        if (dio_readPin(PB,BUTTON_1)== LOW){
-      		result.signal.BTN_1 = HIGH;
-        }
+	else if (current.signal.BTN_1 == HIGH )
+	{
+		dio_resetFlags(PB,BUTTON_1);
+		result.signal.BTN_1=HIGH;
+		result.signal.arrOfPresses[BUTTON_1] += 1;
+		result.signal.AUTO_MODE = NO_AUTO_MODE;
 	}
-    else if ((current.signal.BTN_2 == LOW ) && (diff.signal.BTN_2 == HIGH )){
-        delay_ms(50);
-        if (dio_readPin(PB,BUTTON_2)== LOW){
-      		result.signal.BTN_2 = HIGH;
-        }
+	else if (current.signal.BTN_2 == HIGH )
+	{
+		dio_resetFlags(PB,BUTTON_2);
+		result.signal.BTN_2=HIGH;
+		result.signal.arrOfPresses[BUTTON_2] += 1;
+		result.signal.AUTO_MODE = NO_AUTO_MODE;
 	}
 			
-	previousButtonStage = currentButtonRead;									/*save the current value in the previous one*/
-	return result;
+	result.signal.previous = result.value;									/*save the current value in the previous one*/
+	g_buttonCallBackPtr(result);
 }
 
+void button_setCallBack(void (*a_ptr)(union signalsUnion )){
 	
+	g_buttonCallBackPtr=a_ptr;
+}
--- StrobeLight/StrobeLight/HAL/Button/Button.h
+++ StrobeLight/StrobeLight/HAL/Button/Button.h
@@ -12,25 +12,9 @@
 #include "StdTypes.h"
 #include "Dio_interface.h"
 
+#define FIVE_SEC_COUNT		50
 
 
-
-union signalsUnion{
-	volatile struct  {
-		uint8_t  RIGHT     : 1;		/*PORT B PIN0*/
-		uint8_t  BACK      : 1;		/*PORT B PIN1*/
-		uint8_t  BRAKE     : 1;		/*PORT B PIN2*/
-        uint8_t  BTN_1     : 1;     /*PORT B PIN3*/
-        uint8_t  BTN_2     : 1;     /*PORT B PIN4*/
-        uint8_t  LEFT      : 1;		/*PORT C PIN0*/
-        
-	} signal;
-	uint8_t value ;
-};
-
-union signalsUnion current;
-union signalsUnion diff;
-union signalsUnion result;
 
  /*** Typedefs ***/
  
@@ -46,13 +30,42 @@ typedef enum
             
  } button_t ;
 
+typedef enum
+{
+	SHORT_PRESS,
+	LONG_PRESS
+	} button_pressTime;
+	
+typedef enum{
+	NO_AUTO_MODE,
+	AUTO_MODE_
+	} button_autoMode;
+
+union signalsUnion{
+	volatile struct  {
+		uint8_t  RIGHT		   : 1;		/*PORT B PIN0*/
+		uint8_t  BACK		   : 1;		/*PORT B PIN1*/
+		uint8_t  BRAKE         : 1;		/*PORT B PIN2*/
+		uint8_t  BTN_1         : 1;     /*PORT B PIN3*/
+		uint8_t  BTN_2         : 1;     /*PORT B PIN4*/
+		uint8_t  LEFT          : 1;		/*PORT C PIN0*/
+		button_pressTime  LEFT_PRESSED  : 1;
+		button_pressTime  BRAKE_PRESSED : 1;
+		button_autoMode   AUTO_MODE		: 1;
+		uint8_t	arrOfPresses[6];
+		uint8_t previous     ;
+	} signal;
+	uint8_t value ;
+};
 
 /************* API ***********/
 
 /*initializes all buttons as input pull up*/
 void button_init(void);
 /*reads the buttons' values*/
-union signalsUnion button_read(void);
+void button_detectPress(void);
+
+void button_setCallBack(void (*a_ptr)(union signalsUnion));
 
 
 #endif /* BUTTON_H_ */
